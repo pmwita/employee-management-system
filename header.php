98af0bc66fb6c4f30d183f04e808ec038185e186
pmwita <pmwita2017@gmail.com>
2024-03-17 16:15:59 +0300

    hamburger menu fix

--- header.php
+++ header.php
@@ -85,5 +85,10 @@ $payrollLink = isset($_SESSION['user_id']) ? (isAdmin() ? 'Payroll_management_ad
             </nav>
         </div>
     </header>
+
+    <!-- Link to Bootstrap JS and jQuery (place these before the closing body tag) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
